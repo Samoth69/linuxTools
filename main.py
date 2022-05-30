9ef0102cf6e1dd668c0e4a543507d3c9e1c3850d
Thomas Violent <thomasviolent@gmail.com>
2022-05-30 11:17:03 +0200

    the script now create and load a python venv

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import questionary
 
 
 def main():
-    questionary.select(
+    res = questionary.select(
         "What do you want to do?",
         choices=[
             'Setup auto-update script',
             'Setup correct timezone',
             'Quit'
         ]).ask()  # returns value of selection
-
+    print(res)
 
 if __name__ == "__main__":
     main()
--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# stop le script si une commande plante
+set -e
+
+# load venv
+source .venv/bin/activate
+
 python3 main.py
+
+deactivate
